88faf3894ee9caffffbf4a9acafa94c32d3e4f2d
Pranav Vashi <neobuddy89@gmail.com>
2023-01-29 22:59:06 +0530

    base: Fix disabling power menu completely on secured lockscreen
    * Fixes: https:// github.com/crdroidandroid/issue_tracker/issues/107
    
    Change-Id: I7f316df8914ebcdd33dd5ac86407e6f67bb6baac
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- services/core/java/com/android/server/policy/PhoneWindowManager.java
+++ services/core/java/com/android/server/policy/PhoneWindowManager.java
@@ -1898,7 +1898,7 @@ public class PhoneWindowManager implements WindowManagerPolicy {
     }
 
     void showGlobalActionsInternal() {
-        final boolean keyguardShowing = isKeyguardShowingAndNotOccluded();
+        final boolean keyguardShowing = isKeyguardLocked();
         if (keyguardShowing && isKeyguardSecure(mCurrentUserId) &&
                 !mGlobalActionsOnLockEnable) {
             return;
